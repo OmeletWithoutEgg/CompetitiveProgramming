99dfb11ee5ffd7ae74cc3fd8853fbb214035ccdc
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-08-21 11:07:19 +0800

    CF probs

---
+++ CF/contest/1136/a/a.cpp
@@ -0,0 +1,64 @@
+//   __________________
+//  | ________________ |
+//  ||          ____  ||
+//  ||   /\    |      ||
+//  ||  /__\   |      ||
+//  || /    \  |____  ||
+//  ||________________||
+//  |__________________|
+//  \###################\
+//   \###################\
+//    \        ____       \
+//     \_______\___\_______\
+// An AC a day keeps the doctor away.
+ 
+#pragma GCC optimize("Ofast")
+#pragma loop_opt(on)
+#include <bits/extc++.h>
+#ifdef local
+#define safe std::cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+#define debug(...) qqbx(#__VA_ARGS__, __VA_ARGS__)
+template <typename H, typename ...T> constexpr void qqbx(const char *s, const H &h, T &&...args) {
+    for(; *s && *s != ','; ++s) if(*s != ' ') std::cerr << *s;
+    std::cerr << " = " << h << (sizeof...(T) ? ", " : "\n");
+    if constexpr(sizeof...(T)) qqbx(++s, args...);
+}
+#else
+#include <bits/stdc++.h>
+#define debug(...) ((void)0)
+#define safe ((void)0)
+#endif // local
+#define all(v) begin(v),end(v)
+#define get_pos(v,x) int(lower_bound(begin(v),end(v),x)-begin(v))
+#define sort_uni(v) sort(begin(v),end(v)),v.erase(unique(begin(v),end(v)),end(v))
+#define pb emplace_back
+#define ff first
+#define ss second
+#define mem(v,x) memset(v,x,sizeof v)
+ 
+using namespace std;
+using namespace __gnu_pbds;
+typedef int64_t ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+template <typename T> using max_heap = std::priority_queue<T,vector<T>,less<T> >;
+template <typename T> using min_heap = std::priority_queue<T,vector<T>,greater<T> >;
+template <typename T> using rbt = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+constexpr ld PI = acos(-1), eps = 1e-7;
+constexpr ll N = 1000025, INF = 1e18, MOD = 1000000007, K = 14699, inf = 1e9;
+constexpr inline ll cdiv(ll x, ll m) { return x/m + ((x<0 ^ m>0) && (x%m)); } // ceiling divide
+constexpr inline ll modpow(ll e,ll p,ll m=MOD) { ll r=1; for(e%=m;p;p>>=1,e=e*e%m) if(p&1) r=r*e%m; return r; }
+
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int n;
+    cin >> n;
+    vector<pair<int,int>> chap(n);
+    for(auto &[l, r]: chap) cin >> l >> r;
+    int k, ans = 0;
+    cin >> k;
+    for(auto [l, r]: chap) if(r < k) ++ans;
+    debug(ans);
+    cout << n-ans << '\n';
+}
