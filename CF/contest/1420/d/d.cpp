263e6523fabb0d53bfab30c0b67ba9c3b9f3b8a4
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-09-25 00:04:18 +0800

    cf div.2 round

---
+++ CF/contest/1420/d/d.cpp
@@ -0,0 +1,76 @@
+//   __________________
+//  | ________________ |
+//  ||          ____  ||
+//  ||   /\    |      ||
+//  ||  /__\   |      ||
+//  || /    \  |____  ||
+//  ||________________||
+//  |__________________|
+//  \###################\
+//   \###################\
+//    \        ____       \
+//     \_______\___\_______\
+// An AC a day keeps the doctor away.
+ 
+#ifdef local
+#include <bits/extc++.h>
+#define safe std::cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+#define debug(args...) qqbx(#args, args)
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    std::cerr << "\033[1;32m" << '(' << s << ')' << " = " << '(';
+    constexpr int size = sizeof...(T);
+    int cnt = size, dummy[size] = { (std::cerr << args << (--cnt ? ", " : ")\033[0m\n"), 0)... };
+}
+#else
+#pragma GCC optimize("Ofast")
+#pragma loop_opt(on)
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#define debug(...) ((void)0)
+#define safe ((void)0)
+#endif // local
+#define all(v) begin(v),end(v)
+#define get_pos(v,x) int(lower_bound(begin(v),end(v),x)-begin(v))
+#define sort_uni(v) sort(begin(v),end(v)),v.erase(unique(begin(v),end(v)),end(v))
+#define pb emplace_back
+#define ff first
+#define ss second
+#define mem(v,x) memset(v,x,sizeof v)
+ 
+using namespace std;
+using namespace __gnu_pbds;
+typedef int64_t ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+template <typename T> using max_heap = std::priority_queue<T,vector<T>,less<T> >;
+template <typename T> using min_heap = std::priority_queue<T,vector<T>,greater<T> >;
+template <typename T> using rbt = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+constexpr ld PI = acos(-1), eps = 1e-7;
+constexpr ll N = 1000025, INF = 1e18, MOD = 998244353, K = 14699, inf = 1e9;
+constexpr inline ll cdiv(ll x, ll m) { return x/m + ((x<0 ^ m>0) && (x%m)); } // ceiling divide
+constexpr inline ll modpow(ll e,ll p,ll m=MOD) { ll r=1; for(e%=m;p;p>>=1,e=e*e%m) if(p&1) r=r*e%m; return r; }
+
+signed main() {
+    vector<ll> fr{1};
+    for(int i = 1; i < N; i++) fr.pb(fr.back() * i % MOD);
+    auto choose = [&](int n, int k) -> ll {
+        if(n < 0 || n < k) return 0;
+        return fr[n] * modpow(fr[k] * fr[n-k], MOD-2) % MOD;
+    };
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int,int>> v(n);
+    for(auto &[l, r]: v) cin >> l >> r, ++r;
+    sort(all(v), [](pair<int,int> a, pair<int,int> b) { return a.ff != b.ff ? a.ff < b.ff : a.ss > b.ss; });
+    multiset<int> ms;
+    ll ans = 0;
+    for(int i = 0, sum = 0; i < n; i++) {
+        while(ms.size() && v[i].first >= *ms.begin()) ms.erase(ms.begin()), --sum;
+        ans = (ans + choose(sum, k-1)) % MOD;
+        ++sum;
+        ms.insert(v[i].second);
+    }
+    cout << ans << '\n';
+}
---
+++ CF/contest/1420/e/e.cpp
@@ -0,0 +1,101 @@
+//   __________________
+//  | ________________ |
+//  ||          ____  ||
+//  ||   /\    |      ||
+//  ||  /__\   |      ||
+//  || /    \  |____  ||
+//  ||________________||
+//  |__________________|
+//  \###################\
+//   \###################\
+//    \        ____       \
+//     \_______\___\_______\
+// An AC a day keeps the doctor away.
+ 
+#ifdef local
+#include <bits/extc++.h>
+#define safe std::cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+#define debug(args...) qqbx(#args, args)
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    std::cerr << "\033[1;32m" << '(' << s << ')' << " = " << '(';
+    constexpr int size = sizeof...(T);
+    int cnt = size, dummy[size] = { (std::cerr << args << (--cnt ? ", " : ")\033[0m\n"), 0)... };
+}
+#else
+#pragma GCC optimize("Ofast")
+#pragma loop_opt(on)
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#define debug(...) ((void)0)
+#define safe ((void)0)
+#endif // local
+#define all(v) begin(v),end(v)
+#define get_pos(v,x) int(lower_bound(begin(v),end(v),x)-begin(v))
+#define sort_uni(v) sort(begin(v),end(v)),v.erase(unique(begin(v),end(v)),end(v))
+#define pb emplace_back
+#define ff first
+#define ss second
+#define mem(v,x) memset(v,x,sizeof v)
+ 
+using namespace std;
+using namespace __gnu_pbds;
+typedef int64_t ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+template <typename T> using max_heap = std::priority_queue<T,vector<T>,less<T> >;
+template <typename T> using min_heap = std::priority_queue<T,vector<T>,greater<T> >;
+template <typename T> using rbt = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+constexpr ld PI = acos(-1), eps = 1e-7;
+constexpr ll N = 85, INF = 1e18, MOD = 1000000007, K = 14699, inf = 1e9;
+constexpr inline ll cdiv(ll x, ll m) { return x/m + ((x<0 ^ m>0) && (x%m)); } // ceiling divide
+constexpr inline ll modpow(ll e,ll p,ll m=MOD) { ll r=1; for(e%=m;p;p>>=1,e=e*e%m) if(p&1) r=r*e%m; return r; }
+
+int dp[2][N][N*N][N];
+int a[N], pos[N];
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    /* n=80; */
+    /* for(int i =1; i<=n;i++) a[i]=rand()&1; */
+    int tot = n * (n-1) / 2;
+    int one = 0;
+    for(int i = 1; i <= n; i++) one += a[i];
+    for(int i = 1, j = 1; i <= n; i++) {
+        if(a[i] == 1) {
+            pos[j++] = i;
+        }
+    }
+    for(int j = 0; j <= one; j++) for(int k = 0; k <= tot; k++) for(int last = 0; last <= n; last++) dp[0][j][k][last] = -inf;
+    dp[0][0][0][0] = 0;
+    for(int i = 1; i <= n; i++) {
+        auto &nxt = dp[i&1];
+        auto &DP = dp[~i&1];
+        for(int j = 0; j <= one; j++) {
+            for(int k = 0; k <= tot; k++) {
+                for(int last = 0; last <= n; last++) nxt[j][k][last] = -inf;
+                for(int last = 0; last < i; last++) {
+                    nxt[j][k][last] = DP[j][k][last] + (last - j);
+                }
+                if(j > 0 && k - abs(i-pos[j]) >= 0) {
+                    nxt[j][k][i] = -inf;
+                    for(int last = 0; last < i; last++)
+                        nxt[j][k][i] = max(nxt[j][k][i], DP[j-1][k - abs(i - pos[j])][last]);
+                }
+            }
+        }
+    }
+    int mx = 0;
+    for(int k = 0; k <= tot; k++) {
+        for(int last = 0; last <= n; last++) {
+            mx = max(mx, dp[n & 1][one][k][last]);
+        }
+        cout << mx << (k == tot ? '\n' : ' ');
+    }
+    /*
+     * dp[i][j][k][last] = max(dp[i-1][j][k][last] + (last-j), dp[i-1][j-1][k - dist(last, i)][last]);
+     * dp[i][j][c] = max(dp[i-1][j][c] + (i-j), max{dp[i-1][j'][c-1]})
+     */
+}
