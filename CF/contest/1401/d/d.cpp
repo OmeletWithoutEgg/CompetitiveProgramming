c4e85361537faef3991148492c62338caa5cac75
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-09-24 13:30:30 +0800

    cf prob

---
+++ CF/contest/1401/d/d.cpp
@@ -0,0 +1,100 @@
+//   __________________
+//  | ________________ |
+//  ||          ____  ||
+//  ||   /\    |      ||
+//  ||  /__\   |      ||
+//  || /    \  |____  ||
+//  ||________________||
+//  |__________________|
+//  \###################\
+//   \###################\
+//    \        ____       \
+//     \_______\___\_______\
+// An AC a day keeps the doctor away.
+ 
+#ifdef local
+#include <bits/extc++.h>
+#define safe std::cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+#define debug(args...) qqbx(#args, args)
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    std::cerr << "\033[1;32m" << '(' << s << ')' << " = " << '(';
+    constexpr int size = sizeof...(T);
+    int cnt = size, dummy[size] = { (std::cerr << args << (--cnt ? ", " : ")\033[0m\n"), 0)... };
+}
+#else
+#pragma GCC optimize("Ofast")
+#pragma loop_opt(on)
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#define debug(...) ((void)0)
+#define safe ((void)0)
+#endif // local
+#define all(v) begin(v),end(v)
+#define get_pos(v,x) int(lower_bound(begin(v),end(v),x)-begin(v))
+#define sort_uni(v) sort(begin(v),end(v)),v.erase(unique(begin(v),end(v)),end(v))
+#define pb emplace_back
+#define ff first
+#define ss second
+#define mem(v,x) memset(v,x,sizeof v)
+ 
+using namespace std;
+using namespace __gnu_pbds;
+typedef int64_t ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+template <typename T> using max_heap = std::priority_queue<T,vector<T>,less<T> >;
+template <typename T> using min_heap = std::priority_queue<T,vector<T>,greater<T> >;
+template <typename T> using rbt = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+constexpr ld PI = acos(-1), eps = 1e-7;
+constexpr ll N = 1000025, INF = 1e18, MOD = 1000000007, K = 14699, inf = 1e9;
+constexpr inline ll cdiv(ll x, ll m) { return x/m + ((x<0 ^ m>0) && (x%m)); } // ceiling divide
+constexpr inline ll modpow(ll e,ll p,ll m=MOD) { ll r=1; for(e%=m;p;p>>=1,e=e*e%m) if(p&1) r=r*e%m; return r; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n);
+    for(int i = 1; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+        g[u].pb(v), g[v].pb(u);
+    }
+    vector<int> sz(n, 1);
+    vector<ll> way;
+    function<void(int,int)> dfs = [&](int i, int p) {
+        for(int j: g[i]) if(j != p) {
+            dfs(j, i);
+            way.pb(1LL * (n-sz[j]) * sz[j]);
+            sz[i] += sz[j];
+        }
+    };
+    dfs(0, -1);
+    int m;
+    cin >> m;
+    vector<int> pr(m);
+    for(int &x: pr) cin >> x;
+    if(m <= n-1) {
+        pr.resize(n-1, 1);
+        sort(pr.begin(), pr.end());
+    } else {
+        sort(pr.begin(), pr.end());
+        while(pr.size() > n-1) {
+            int a = pr.back(); pr.pop_back();
+            int b = pr.back(); pr.pop_back();
+            pr.pb(1LL * a * b % MOD);
+        }
+    }
+    sort(way.begin(), way.end());
+    for(ll &x: way) x %= MOD;
+    ll ans = 0;
+    for(int i = 0; i < n-1; i++) ans = (ans + pr[i] * way[i]) % MOD;
+    cout << ans << '\n';
+}
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--) solve();
+}
