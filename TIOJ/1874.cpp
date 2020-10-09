a262081ffb4904912f566693836f076880d72b20
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-10-09 14:35:13 +0800

    get topcoder

--- TIOJ/1874.cpp
+++ TIOJ/1874.cpp
@@ -1,11 +1,10 @@
-#pragma g++ optimize("O3")
+#pragma GCC optimize("Ofast")
 #pragma loop_opt(on)
 #include <cstdio>
-#include <vector>
 inline char readchar() {
     constexpr int B = 1<<20;
     static char buf[B], *p=buf, *q=buf;
-    if(p == q && (q=(p=buf)+fread(buf,1,B,stdin)) == buf) return EOF;
+    if(p == q && (q=(p=buf)+fread_unlocked(buf,1,B,stdin)) == buf) return EOF;
     return *p++;
 }
 inline int nextint() {
@@ -14,9 +13,27 @@ inline int nextint() {
     while(c >= '0') x=x*10+(c^'0'), c=readchar();
     return x;
 }
-const int N = 10000001;
-std::vector<int> prs;
-int d[N];
+struct FastOut {
+    char *buf;
+    int q;
+    const int B;
+    inline void writeln(int x) {
+        static char stk[20];
+        char p = 0;
+        if(!x) stk[p++] = '0';
+        while(x) stk[p++] = x%10^'0', x/=10;
+        while(p) buf[q++] = stk[--p];
+        buf[q++] = '\n';
+        if(q + 20 >= B) fwrite_unlocked(buf, 1, q, stdout), q = 0;
+    }
+    FastOut(int B = 1<<20) : B(B), q(0), buf(new char[B]){}
+    ~FastOut() {
+        fwrite_unlocked(buf, 1, q, stdout);
+        delete [] buf;
+    }
+};
+const int N = 10000001, K = 65536;
+short d[N], prs[K], it;
 char np[N];
 signed main() {
     d[1] = 1;
@@ -24,14 +41,14 @@ signed main() {
         if(!np[i]) {
             np[i] = 1;
             d[i] = 2;
-            if(1LL*i*i < N) prs.emplace_back(i);
+            if(i < K) prs[it++] = i;
         }
-        for(long long p: prs) {
-            if(i*p >= N) break;
+        for(int j = 0; j < it && i*prs[j] < N; j++) {
+            int p = prs[j];
             if(i%p) {
                 np[i*p] = 1;
                 d[i*p] = d[i]*d[p];
-            }else {
+            } else {
                 np[i*p] = np[i]+1;
                 d[i*p] = d[i]/(np[i]+1)*(np[i]+2);
                 break;
@@ -39,8 +56,9 @@ signed main() {
         }
     }
     int q = nextint();
+    FastOut ouf;
     while(q--) {
         int n = nextint();
-        printf("%d\n", d[n]);
+        ouf.writeln(d[n]);
     }
 }
