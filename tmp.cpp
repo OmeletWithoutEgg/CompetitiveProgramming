d2252180da1a60ba042b000acd1fb2e3dafbb0a3
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-11-08 11:38:12 +0800

    HPCW

--- tmp.cpp
+++ tmp.cpp
@@ -1,18 +1,8 @@
-#include <bits/stdc++.h>
-#ifdef local
-#define debug(args...) qqbx(#args, args)
-#define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
-template <typename ...T> void qqbx(const char *s, T ...args) {
-    int cnt = sizeof...(T);
-    (std::cerr << "(" << s << ") = (" , ... , (std::cerr << args << (--cnt ? ", " : ")\n")));
-}
-#else
-#define debug(...) ((void)0)
-#define safe ((void)0)
-#endif // local
-#define all(v) begin(v),end(v)
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
+using ll = long long;
+const ll inf = 1e2;
 
 signed main() {
     ios_base::sync_with_stdio(0), cin.tie(0);
