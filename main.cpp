0a23efe16f57adcaf33a2c6e6a2290566a132667
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-11-08 11:38:40 +0800

    IDK

--- main.cpp
+++ main.cpp
@@ -1,80 +1,65 @@
-#pragma GCC optimize("Os")
-#include <unistd.h>
+//   __________________
+//  | ________________ |
+//  ||          ____  ||
+//  ||   /\    |      ||
+//  ||  /__\   |      ||
+//  || /    \  |____  ||
+//  ||________________||
+//  |__________________|
+//  \###################\
+//   \###################\
+//    \        ____       \
+//     \_______\___\_______\
+// An AC a day keeps the doctor away.
+ 
+#ifdef local
+#include <bits/extc++.h>
+#define safe std::cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+#define debug(args...) qqbx(#args, args)
+using ost = std::ostream;
+#define DESTL(STL, BEG, END, OUT) \
+    template <typename ...T> ost& operator<<(ost &O, std::STL<T...> v) { int f=0; for(auto x: v) O << (f++ ? ", " : BEG) << OUT; return O << END; }
+DESTL(deque, "[", "]", x); DESTL(vector, "[", "]", x);
+DESTL(set, "{", "}", x); DESTL(multiset, "{", "}", x); DESTL(unordered_set, "{", "}", x);
+DESTL(map , "{", "}", x.first << ":" << x.second); DESTL(unordered_map , "{", "}", x.first << ":" << x.second);
+template <typename U, typename V> ost& operator<<(ost &O, std::pair<U,V> p) { return O << '(' << p.first << ',' << p.second << ')'; }
+template <typename T, size_t N> ost& operator<<(ost &O, std::array<T,N> a) { int f=0; for(T x: a) O << (f++ ? ", " : "[") << x; return O << "]"; }
+template <typename T, size_t ...I> ost& prtuple(ost &O, T t, std::index_sequence<I...>) { return (..., (O << (I ? ", " : "(") << std::get<I>(t))), O << ")"; }
+template <typename ...T> ost& operator<<(ost &O, std::tuple<T...> t) { return prtuple(O, t, std::make_index_sequence<sizeof...(T)>()); }
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    int cnt = sizeof...(T);
+    (std::cerr << "\033[1;32m(" << s << ") = (" , ... , (std::cerr << args << (--cnt ? ", " : ")\033[0m\n")));
+}
+#else
+#pragma GCC optimize("Ofast")
+#pragma loop_opt(on)
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#define debug(...) ((void)0)
+#define safe ((void)0)
+#endif // local
+#define all(v) begin(v),end(v)
+#define get_pos(v,x) int(lower_bound(begin(v),end(v),x)-begin(v))
+#define sort_uni(v) sort(begin(v),end(v)),v.erase(unique(begin(v),end(v)),end(v))
+#define pb emplace_back
+#define ff first
+#define ss second
+#define mem(v,x) memset(v,x,sizeof v)
+ 
+using namespace std;
+using namespace __gnu_pbds;
+typedef int64_t ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+template <typename T> using max_heap = std::priority_queue<T,vector<T>,less<T> >;
+template <typename T> using min_heap = std::priority_queue<T,vector<T>,greater<T> >;
+template <typename T> using rbt = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+constexpr ld PI = acos(-1), eps = 1e-7;
+constexpr ll N = 1000025, INF = 1e18, MOD = 1000000007, K = 14699, inf = 1e9;
+constexpr inline ll cdiv(ll x, ll m) { return x/m + (x%m ? (x<0) ^ (m>0) : 0); } // ceiling divide
+constexpr inline ll modpow(ll e,ll p,ll m=MOD) { ll r=1; for(e%=m;p;p>>=1,e=e*e%m) if(p&1) r=r*e%m; return r; }
 
-inline char readchar() {
-    constexpr int B = 1<<10;
-    static char buf[B], *p, *q;
-    if(p == q && (q=(p=buf)+read(0, buf, B)) == buf) return -1;
-    return *p++;
-}
-inline int nextint() {
-    int x = 0, c = readchar();
-    while(c < '0' && c != -1) c = readchar();
-    while(c >= '0') x=x*10+(c^'0'), c=readchar();
-    return x;
-}
-inline void writestr(const char *s) {
-    int len = 0;
-    for(; s[len]; ++len);
-    write(1, s, len);
-}
-inline void writeint(int x) {
-    static char stk[20], buf[20];
-    char p = 0, q = 0;
-    if(!x) stk[p++] = '0';
-    while(x) stk[p++] = x%10^'0', x/=10;
-    while(p) buf[q++] = stk[--p];
-    write(1, buf, q);
-}
-
-const int N = 225;
-int dis[N];
-int g[N][N], my[N];
-int n, m;
-int q[N];
-int prv[N], py[N];
-bool BFS(int s) {
-    int head = 0, tail = 0;
-    q[tail++] = s;
-    dis[s] = 0;
-    while(head != tail) {
-        int i = q[head++];
-        for(int j = 0; j < m; j++) if(!g[i][j]) {
-            int x = my[j];
-            if(x == -1) {
-                my[j] = i;
-                for(int x = i; x != s; x = prv[x]) my[py[x]] = prv[x];
-                return true;
-            }
-            if(dis[x] == -1) {
-                dis[x] = dis[i] + 1;
-                prv[x] = i;
-                py[x] = j;
-                q[tail++] = x;
-            }
-        }
-    }
-    return false;
-}
 signed main() {
-    int tc = 0;
-    while(n = nextint()) {
-        m = nextint();
-        for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) g[i][j] = nextint();
-        int ans = 0;
-        for(int i = 0; i < m; i++) my[i] = -1;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) dis[j] = -1;
-            if(BFS(i)) {
-                ++ans;
-            }
-        }
-        ans = (n+m-ans) * 2;
-        writestr("Case ");
-        writeint(++tc);
-        writestr(": ");
-        writeint(ans);
-        writestr("\n");
-        /* printf("Case %d: %d\n", ++tc, ans); */
-    }
+    ios_base::sync_with_stdio(0), cin.tie(0);
 }
