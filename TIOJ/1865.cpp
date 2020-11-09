ff866eee3ba8e117e8c48c3f87824606d9451007
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-11-09 10:55:37 +0800

    AC TIOJ 1865

--- TIOJ/1865.cpp
+++ TIOJ/1865.cpp
@@ -5,15 +5,16 @@
 #include <ctime>
 #include <cstdio>
 #include <algorithm>
+#include <cassert>
 long long v[1000];
 int cnt;
 int GetN() {
-    srand(time(0));
+    srand(7122);
     for(int i = 0; i < 1000; i++) v[i] = rand()%25+1;
     return 1000;
 }
 bool Magic_Isdivide(int x) {
-    if(x+1 >= 1000) abort();
+    assert(0 <= x && x < 1000);
     ++cnt;
     return v[x+1]%v[x] == 0;
 }
@@ -33,10 +34,10 @@ signed main() {
     int n = GetN();
     for(int i = 0; i < n; i++) {
         for(int j = i-1; j >= 0; j--) {
-            if(!Magic_Isdivide(j))
+            /* if(!Magic_Isdivide(j)) */
                 Magic_Operate(j);
-            else
-                break;
+            /* else */
+                /* break; */
         }
     }
     End();
