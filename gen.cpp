81c71434791650cb38eb627e45b53ea397b7dc1b
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-11-09 11:01:02 +0800

    trying hack random solution to ZCK prob

--- gen.cpp
+++ gen.cpp
@@ -1,52 +1,12 @@
 #include <bits/stdc++.h>
-#define y1 y_1
-#define pdi pair<double,int>
-#define ff first
-#define ss second
+
 using namespace std;
- 
-const int MAXN = 2000;
-const int MAXC = 200000;
-const double INF = 1e18;
-int Q;
-string testcase;
-struct line{
-  double a, b;
-} p[MAXN+1], tmp;
-int cnt;
-int x1, x2, y1, y2;
-pdi mmax;
- 
-double calc(int id, int x) {
-  return p[id].a * x + p[id].b;
-}
- 
-signed main(int argc, char *argv[]) {
-  srand(time(NULL));
-  testcase = argv[1];
-  ofstream gen(testcase + ".in");
-  ofstream ans(testcase + ".out");
-  Q = atoi(argv[2]);
-  gen << Q << "\n";
-  while (Q--) {
-    if (!cnt || rand() % 2) {
-      x1 = 1, x2 = MAXC;
-      y1 = rand() % MAXC + 1;
-      y2 = rand() % MAXC + 1;
-      gen << "add " << x1 << " " << y1 << " " << x2 << " " << y2 << "\n";
-      if (x1 == x2)
-        tmp.a = 0, tmp.b = max(y1, y2);
-      else
-        tmp.a = 1.0 * (y1 - y2) / (x1 - x2), tmp.b = y1 - x1 * tmp.a;
-      p[++cnt] = tmp;
-    } else {
-      x1 = rand() % MAXC + 1;
-      gen << "qry " << x1 << "\n";
-      mmax = pdi(-INF, 0);
-      for (int i = 1; i <= cnt; i++)
-        mmax = max(mmax, pdi(calc(i, x1), i));
-      ans << fixed << setprecision(6) << mmax.ff << " " << mmax.ss << "\n";
+
+signed main() {
+    for(int i = 0, s = 0; i < 8; i++, s += 6) {
+#define ou(a, b) cout << s+a << ' ' << s+b << '\n';
+        ou(0, 2);
+        ou(1, 4);
+        ou(3, 5);
     }
-  }
-  return 0;
 }
--- sol.cpp
+++ sol.cpp
@@ -1,136 +1,55 @@
 #include <bits/stdc++.h>
-#define pb emplace_back
+#ifdef local
 #define debug(args...) qqbx(#args, args)
-template <typename H, typename ...T> void qqbx(const char *s, const H &h, T&& ...args) {
-    for(; *s && *s != ','; ++s) if(*s != ' ') std::cerr << *s;
-    std::cerr << " = " << h << (sizeof...(T) ? ", " : "\n");
-    if constexpr(sizeof...(T)) qqbx(++s, args...);
+#define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    int cnt = sizeof...(T);
+    (std::cerr << "(" << s << ") = (" , ... , (std::cerr << args << (--cnt ? ", " : ")\n")));
 }
-using namespace std;
-typedef long long ll;
-const int N = 100025, inf = 1e9;
+#else
+#define debug(...) ((void)0)
+#define safe ((void)0)
+#endif // local
+#define all(v) begin(v),end(v)
+#define pb emplace_back
 
-struct LinkCutTree {
-    struct node {
-        int pa, ch[2];
-        ll a, b, val;
-        node(int p=0, int v=0) : pa(p), ch{0,0}, a(0), b(inf), val(v) {}
-    } T[N];
-    bool dir(int x) { return T[T[x].pa].ch[1] == x; }
-    bool isroot(int x) { return !x || T[T[x].pa].ch[dir(x)] != x; }
-    void rot(int x) {
-        int y = T[x].pa, z = T[y].pa, d = dir(x);
-        T[x].pa = z;
-        if(!isroot(y)) T[z].ch[dir(y)] = x;
-        if(T[x].ch[!d]) T[T[x].ch[!d]].pa = y;
-        T[y].ch[d] = T[x].ch[!d];
-        T[y].pa = x;
-        T[x].ch[!d] = y;
-    }
-    void upd(int x, ll a, ll b) {
-        T[x].val = min(T[x].val + a, b);
-        T[x].a += a;
-        T[x].b = min(T[x].b + a, b);
-    }
-    void push(int x) {
-        if(!isroot(x)) push(T[x].pa);
-        if(T[x].a == 0 && T[x].b == inf) return;
-        for(int d: {0, 1}) if(T[x].ch[d]) upd(T[x].ch[d], T[x].a, T[x].b);
-        T[x].a = 0;
-        T[x].b = inf;
-    }
-    void splay(int x) {
-        push(x);
-        while(!isroot(x)) {
-            int y = T[x].pa;
-            if(!isroot(y))
-                rot(dir(x) ^ dir(y) ? x : y);
-            rot(x);
-        }
-    }
-    int access(int x) {
-        int last = 0;
-        while(x) {
-            splay(x);
-            T[x].ch[1] = last;
-            last = x;
-            x = T[x].pa;
-        }
-        return last;
-    }
-    void modify(int x, int y, int a, int b) {
-        access(x);
-        int LCA = access(y);
-        splay(x);
-        if(x == LCA) {
-            upd(T[x].ch[1], a, b);
-        } else {
-            upd(T[LCA].ch[1], a, b);
-            upd(x, a, b);
-        }
-    }
-    ll eval(int x) {
-        splay(x);
-        return T[x].val;
-    }
-} lct;
-int eid[N];
-signed main () {
+using namespace std;
+const int N = 24;
+
+int p[N], l[N], r[N], g[N];
+signed main() {
     ios_base::sync_with_stdio(0), cin.tie(0);
     int n;
     cin >> n;
-    vector<vector<tuple<int,int,int>>> g(n+1); // cost, edgeid, toid
-    for(int i = 1; i < n; i++) {
-        int a, b, w;
-        cin >> a >> b >> w;
-        g[a].pb(w, i, b);
-        g[b].pb(w, i, a);
-    }
-    function<void(int, int)> dfs = [&](int i, int p) {
-        for(auto [c, id, j]: g[i]) {
-            if(j != p) {
-                lct.T[j] = LinkCutTree::node(i, c);
-                eid[id] = j;
-                dfs(j, i);
+    for(int i = 0; i < n; i++) cin >> l[i] >> r[i];
+    for(int i = 0; i < n; i++) {
+        g[i] = 1 << i;
+        for(int j = 0; j < n; j++) {
+            if(j == i) continue;
+            if((l[i] < l[j] && l[j] < r[i]) ^ (l[i] < r[j] && r[j] < r[i])) {
+                g[i] |= 1 << j;
             }
         }
-    };
-    dfs(1, 0);
-    int q;
-    cin >> q;
-    while(q--) {
-        int t;
-        cin >> t;
-        if(t == 1) {
-            int x, y, a, b;
-            cin >> x >> y >> a >> b;
-            lct.modify(x, y, a, b);
-        } else if(t == 2) {
-            int id;
-            cin >> id;
-            // --id / ++id ??
-            debug(eid[id]);
-            cout << lct.eval(eid[id]) << '\n';
-        }
     }
+    int ans = n;
+    iota(p, p+n, 0);
+    mt19937 rng(7122);
+    for(int c = 0; c < 50; c++) {
+        shuffle(p, p+n, rng);
+        vector<int> now;
+        for(int i = 0; i < n; i++) {
+            bool found = false;
+            for(int &x: now) {
+                if(~x >> p[i] & 1) {
+                    x |= g[p[i]];
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) now.pb(g[p[i]]);
+        }
+        ans = min(ans, int(now.size()));
+        /* next_permutation(p, p+n); */
+    }
+    cout << ans << '\n';
 }
-
-/*
-5
-1 5 3
-1 4 6
-4 2 3
-3 1 9
-4
-1 1 4 10 9
-1 5 3 4 2
-1 3 2 6 9
-2 3
-
-  *2   *8
-5 -- 1 -- 3
-     | *9
-     4
-     | *9
-     2
-*/
