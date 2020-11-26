260a45d4f0214864fa743f39c47abc3030693246
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-11-26 11:43:51 +0800

    random probs

--- gen.cpp
+++ gen.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 signed main() {
-    for(int i = 0, s = 0; i < 8; i++, s += 6) {
-#define ou(a, b) cout << s+a << ' ' << s+b << '\n';
-        ou(0, 2);
-        ou(1, 4);
-        ou(3, 5);
+    int n = 3000;
+    cout << n << ' ' << n << '\n';
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "RGW"[(i+j)%3];
+        }
+        cout << '\n';
     }
 }
--- jizz.cpp
+++ jizz.cpp
@@ -1,105 +1,61 @@
-#include <bits/stdc++.h>
-#ifdef local
-#define debug(...) qqbx(#__VA_ARGS__, __VA_ARGS__)
-void qqbx(const char *s) {}
-template <typename H, typename ...T> void qqbx(const char *s, const H &h, T ...args) {
-    for(; *s && *s != ','; ++s) if(*s != ' ') std::cerr << *s;
-    std::cerr << " = " << h << (sizeof...(T) ? ", " : "\n");
-    if (sizeof...(T)) qqbx(++s, args...);
-}
-#else
-#define debug(...) ((void)0)
-#endif // local
-#define pb emplace_back
-#define ff first
-#define ss second
-#define all(v) begin(v),end(v)
-#define get_pos(v, x) int(lower_bound(begin(v),end(v),x) - begin(v))
-
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <vector>
 using namespace std;
-typedef int64_t ll;
-typedef long double ld;
-const int N = 200025, MOD = 1000000007;
-const ld PI = acos(-1);
-
-/*
-bool check(pair<int,int> a, pair<int,int> b) {
-    static const auto in = [](pair<int,int> I, int x) {
-        if(I.ff < I.ss) return I.ff < x && x < I.ss;
-        return I.ff < x || x < I.ss;
-    };
-    return in(a, b.ff) || in(a, b.ss) || in(b, a.ff) || in(b, a.ss);
-}
-*/
-struct FenwickTree : vector<int> {
-    FenwickTree(int n) : vector<int>(n) {}
-    void add(int p, int d) {
-        for(++p; p <= size(); p += p&-p) at(p-1) += d;
+const int N = 505;
+int vis[N];
+vector<int> g[N];
+int tot;
+int ans;
+void dfs(int cu,int sum)
+{
+    if (sum > ans) return;
+    if (cu > tot)
+    {
+        ans = sum;
+        return;
     }
-    int query(int p) {
-        int res = 0;
-        for(++p; p > 0; p -= p&-p) res += at(p-1);
-        return res;
-    }
-};
-signed main() {
-#ifndef local
-    assert( freopen("sight.in", "r", stdin) && freopen("sight.out", "w", stdout) );
-#endif // local
-    ios_base::sync_with_stdio(0), cin.tie(0);
-    int n, R;
-    cin >> n >> R;
-    vector<pair<ld,ld>> I(n);
-    vector<ld> u;
-    for(int i = 0; i < n; i++) {
-        ll x, y;
-        cin >> x >> y;
-        ld theta = atan2(y, x);
-        ld D = sqrt(x*x+y*y);
-        ld phi = acos(R / D);
-        ld l = theta - phi, r = theta + phi;
-        if(l < 0) l += PI * 2;
-        if(r < 0) r += PI * 2;
-        if(l >= PI*2) l -= PI*2;
-        if(r >= PI*2) r -= PI*2;
-        I[i] = {l, r};
-        u.pb(l), u.pb(r);
-    }
-    sort(all(u));
-    assert(unique(u.begin(), u.end()) == u.end());
-    vector<pair<int,int>> v;
-    vector<tuple<int,int,int>> evt;
-    for(int i = 0; i < n; i++) {
-        int l = get_pos(u, I[i].ff);
-        int r = get_pos(u, I[i].ss);
-        // debug(l, r);
-        if(l < r)
-            v.pb(l, r), evt.pb(l, r, 1);
-        else
-            evt.pb(r, l, 0);
-    }
-
-    ll ans = 0;
-    sort(all(v));
-    FenwickTree fwt(u.size());
-    for(auto p: v) {
-        int l, r;
-        tie(l, r) = p;
-        ans += fwt.query(l);
-        fwt.add(r, 1);
-    }
-    fwt = FenwickTree(u.size());
-    // b1 < a1 < a2 < b2
-    sort(all(evt));
-    for(auto t: evt) {
-        int a, b, c;
-        tie(a, b, c) = t;
-        if(c) {
-            ans += fwt.query(u.size()-1) - fwt.query(b);
-        } else {
-            fwt.add(b, 1);
+    if (vis[cu]) dfs(cu+1,sum);
+    else
+    {
+        vis[cu]++;
+        dfs(cu+1,sum+1);
+        vis[cu]--;
+        for (auto x : g[cu])
+        {
+            if (!vis[x]) sum++;
+            vis[x]++;
+        }
+        dfs(cu+1,sum);
+        for (auto x : g[cu])
+        {
+            vis[x]--;
+            if (!vis[x]) sum--;
         }
     }
-    ans = 1LL * n * (n-1) / 2 - ans;
-    cout << ans << '\n';
+}
+int main()
+{
+    int n,m;
+    while (scanf("%d%d",&n,&m) != EOF)
+    {
+        tot = 0;
+        ans = 10000;
+        memset(vis,0,sizeof(vis));
+        for (int i = 1;i <= n;i++) g[i].clear();
+        for (int i = 0;i < m;i++)
+        {
+            int x,y;
+            if(scanf("%d%d",&x,&y) != 2) exit(0);
+            g[x].push_back(y);
+            g[y].push_back(x);
+        }
+        tot = min(30,n);
+        ans = tot;
+        dfs(1,0);
+        printf("tot = %d\n", tot);
+        printf("%d\n",ans);
+    }
+    return 0;
 }
