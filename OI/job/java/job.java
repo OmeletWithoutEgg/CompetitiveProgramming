461def406a804c6885f4627d3a406a043bd813b1
OmeletWithoutEgg <2qbingxuan@gmail.com>
2020-12-01 14:32:40 +0800

    remove java and pascal files

--- OI/job/java/job.java
+++
@@ -1,5 +0,0 @@
-class job {
-	public int scheduling_cost(int[] p, int[] u, int[] d) {
-		return 0;
-	}
-}
--- OI/shoes/java/shoes.java
+++
@@ -1,5 +0,0 @@
-public class shoes {
-	public long count_swaps(int[] s) {
-		return 1;
-	}
-}
