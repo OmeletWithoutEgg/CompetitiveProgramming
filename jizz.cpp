79280937d21daaf8d84617d308525edb9715213b
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-01-05 23:40:59 +0800

    dreamoon cont.

--- jizz.cpp
+++ jizz.cpp
@@ -1,28 +1,70 @@
 #include <bits/stdc++.h>
+#ifdef local
+#define debug(args...) qqbx(#args, args)
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    int cnt = sizeof...(T);
+    ((std::cerr << "(" << s << ") = (") , ... , (std::cerr << args << (--cnt ? ", " : ")\n")));
+}
+#else
+#define debug(...) ((void)0)
+#endif
 #define pb emplace_back
+#define all(v) begin(v),end(v)
+#define sort_uni(v) sort(begin(v),end(v)),v.erase(unique(begin(v),end(v)),end(v))
 
 using namespace std;
-using ld = long double;
 using ll = long long;
+const int N = 1000025, inf = 1e9;
 
-signed main() {
-#ifndef local
-    freopen("balance.in", "r", stdin);
-    freopen("balance.out", "w", stdout);
-#endif // local
-    ios_base::sync_with_stdio(0), cin.tie(0);
-    int n;
-    cin >> n;
-    vector<int> f(n+2);
-    vector<ld> ans(n+2);
-    for(int i = 1; i <= n; i++) cin >> f[i];
-    for(int i = 1; i <= n; i++) {
-        ld res = -1e18;
-        for(int j = i; j <= n+1; j++) {
-            res = max(res, (f[j] - ans[i-1]) / ld(j-i+1));
-        }
-        ans[i] = ans[i-1] + res;
+vector<pair<ll,int>> v;
+vector<ll> ans;
+void dfs(ll lim, ll prod, int i) {
+    ans.pb(prod);
+    if(i == v.size()) return;
+    auto [p, maxC] = v[i];
+    for(int c = 0; c <= maxC; c++) {
+        dfs(lim, prod, i+1);
+        if(prod > lim / p) break;
+        prod *= p;
     }
-    for(int i = 1; i <= n; i++)
-        cout << ll(floor(ans[i] * 100000 + 1e-7)) << '\n';
+}
+vector<int> prs;
+bool sv[N];
+void solve() {
+    ll lim;
+    ll a, b;
+    cin >> lim >> a >> b;
+    map<ll,int> mp;
+    for(int p: prs) {
+        while(a % p == 0) a /= p, ++mp[p];
+        if(p*p >= a) break;
+    }
+    for(int p: prs) {
+        while(b % p == 0) b /= p, ++mp[p];
+        if(p*p >= b) break;
+    }
+    if(a > 1) mp[a] += 1;
+    if(b > 1) mp[b] += 1;
+    v.clear();
+    for(auto [p, c]: mp) v.pb(p, c);
+    ans.clear();
+    dfs(lim, 1, 0);
+    sort_uni(ans);
+    for(int i = 0; i < ans.size(); i++)
+        cout << ans[i] << (i+1==ans.size() ? '\n' : ' ');
+}
+signed main() {
+    for(int i = 2; i < N; i++) {
+        if(!sv[i]) prs.pb(i);
+        for(int p: prs) {
+            if(i*p >= N) break;
+            sv[i*p] = true;
+            if(i%p == 0) break;
+        }
+    }
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
 }
--- tmp.cpp
+++ tmp.cpp
@@ -1,37 +1,56 @@
 #include <bits/stdc++.h>
-#define TIME Timer __timer(__PRETTY_FUNCTION__)
-using namespace std;
-class Timer {
-private:
-    std::string title;
-    chrono::high_resolution_clock::time_point start;
-public:
-    Timer(std::string t) :
-        title(t), start(chrono::high_resolution_clock::now()) {}
-    ~Timer() {
-        auto finish = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::microseconds>(finish - start).count();
-        double ms = double(duration) * 0.001;
-        std::cerr << "Timer: " << title << " takes " << ms << " ms to finish.\n";
-    }
-};
-
-using namespace std;
-
-signed main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for(int &x: v) cin >> x;
-    TIME;
-// #define SORT_UNIQUE
-#ifdef SORT_UNIQUE
-    vector<int> u = v;
-    sort(u.begin(), u.end());
-    u.erase(unique(u.begin(), u.end()), u.end());
+#ifdef local
+#define debug(args...) qqbx(#args, args)
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    int cnt = sizeof...(T);
+    ((std::cerr << "(" << s << ") = (") , ... , (std::cerr << args << (--cnt ? ", " : ")\n")));
+}
 #else
-    unordered_set<int> s(v.begin(), v.end());
-    vector<int> u(s.begin(), s.end());
+#define debug(...) ((void)0)
 #endif
-    cout << accumulate(u.begin(), u.end(), 0LL) << '\n';
+#define pb emplace_back
+#define all(v) begin(v),end(v)
+
+using namespace std;
+using ll = long long;
+const int N = 200025, inf = 1e9, MOD = 998244353;
+
+int a[21][21], pos[21][21];
+int dp[21][1<<20];
+int dp2[1<<20];
+void modadd(int &a, int v) {
+    a += v;
+    a %= MOD;
+}
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int n, m;
+    cin >> m >> n;
+    for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) cin >> a[i][j], --a[i][j], pos[i][a[i][j]] = j;
+    dp[0][0] = 1;
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < (1<<n); j++) dp2[j] = dp[i][j];
+        for(int t = 0; t+1 < n; t++) {
+            for(int j = 0; j < (1<<n); j++) {
+                if(!(j >> t & 1) && !(j >> (t+1) & 1)) {
+                    int k = j | (1<<t) | (1<<(t+1));
+                    modadd(dp2[k], dp2[j]);
+                }
+            }
+        }
+        // for(int j = 0; j < (1<<n); j++) cout << dp[i][j] << ' ';
+        // cout << '\n';
+        // for(int j = 0; j < (1<<n); j++) cout << dp2[j] << ' ';
+        // cout << '\n';
+        for(int j = 0; j < (1<<n); j++) {
+            int nxt = 0;
+            for(int t = 0; t < n; t++) if(!(j >> t & 1)) {
+                int p = i+1<m ? pos[i+1][a[i][t]] : t;
+                nxt |= 1 << p;
+            }
+            // debug(bitset<4>(j), bitset<4>(nxt));
+            modadd(dp[i+1][nxt], dp2[j]);
+        }
+    }
+    cout << dp2[(1<<n)-1] << '\n';
 }
