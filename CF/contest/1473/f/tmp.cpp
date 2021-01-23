157fa5366ba37dc32614b55b76338934eacccd34
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-01-23 12:04:41 +0800

    wrong flow sol.

--- CF/contest/1473/f/tmp.cpp
+++ CF/contest/1473/f/tmp.cpp
@@ -15,12 +15,22 @@
 #include <queue>
 #include <iostream>
 #include <algorithm>
+#define debug(args...) qqbx(#args, args)
+#define TAK(args...) std::ostream& operator<<(std::ostream &O, args)
+#define NIE(STL, BEG, END, OUT) template <typename ...T> TAK(std::STL<T...> v) \
+    { O << BEG; int f=0; for(auto e: v) O << (f++ ? ", " : "") << OUT; return O << END; }
+NIE(vector, "[", "]", e)
+template <typename ...T> void qqbx(const char *s, T ...args) {
+    int cnt = sizeof...(T);
+    if(!cnt) return std::cerr << "\033[1;32m() = ()\033\[0m\n", void();
+    (std::cerr << "\033[1;32m(" << s << ") = (" , ... , (std::cerr << args << (--cnt ? ", " : ")\033[0m\n")));
+}
 
 using std::queue, std::cin, std::cout, std::min;
 
 const int N = 3025, inf = 1e9;
 
-int dis[N], prv[N], dist, prvt;
+int dis[N], dist;
 int n;
 int a[N], b[N];
 int rest[N*N/2];
@@ -28,7 +38,7 @@ int encode(int a, int b) { // b < a
     return a*(a-1)/2 + b;
 }
 bool BFS() {
-    for(int i = 0; i < n; i++) dis[i] = -1, prv[i] = -1;
+    for(int i = 0; i < n; i++) dis[i] = -1;
     queue<int> q;
     for(int i = 0; i < n; i++)
         if(b[i] > 0)
@@ -38,42 +48,59 @@ bool BFS() {
         for(int j = 0; j < i; j++)
             if(a[i] % a[j] == 0) {
                 if(dis[j] == -1)
-                    dis[j] = dis[i] + 1, prv[j] = i, q.push(j);
+                    dis[j] = dis[i] + 1, q.push(j);
             }
         for(int j = i+1; j < n; j++)
             if(rest[encode(j, i)] > 0 && a[j] % a[i] == 0)
                 if(dis[j] == -1)
-                    dis[j] = dis[i] + 1, prv[j] = i, q.push(j);
+                    dis[j] = dis[i] + 1, q.push(j);
     }
     dist = -1;
     for(int i = 0; i < n; i++) if(dis[i] != -1 && b[i] < 0) {
         if(dist == -1 || dist > dis[i] + 1)
-            dist = dis[i], prvt = i;
+            dist = dis[i] + 1;
     }
     return dist != -1;
+}
+int cur[N];
+int DFS(int i = -1, int lim = inf) {
+    int ans = 0;
+    if(i == -1) {
+        for(int j = 0; j < n; j++) cur[j] = 0;
+        for(int j = 0; j < n && lim > 0; j++)
+            if(b[j] > 0 && dis[j] == 1) {
+                int f = DFS(j, b[j]);
+                ans += f;
+                lim -= f;
+                b[j] -= f;
+            }
+        return ans;
+    }
+    // if(vis[i]) return 0;
+    // vis[i] = true;
+    if(b[i] < 0 && dis[i] + 1 == dist) {
+        int f = min(lim, -b[i]);
+        ans += f;
+        lim -= f;
+        b[i] += f;
+    }
+    for(int &j = cur[i]; j < n && lim > 0; j++)
+        if(j != i && (j < i ? a[i] % a[j] == 0 : a[j] % a[i] == 0) && dis[j] == dis[i] + 1) {
+            int cap = j < i ? inf : rest[encode(j, i)];
+            int f = DFS(j, min(lim, cap));
+            ans += f;
+            lim -= f;
+            if(j > i)
+                rest[encode(j, i)] -= f;
+            else
+                rest[encode(i, j)] += f;
+        }
+    return ans;
 }
 int maxFlow() {
     int ans = 0;
     while(BFS()) {
-        int f = abs(b[prvt]), x, last;
-        for(x = prv[prvt], last = prvt; x != -1; last = x, x = prv[x]) {
-            if(last < x) {
-                // f = min(f, rest[encode(x, last)]);
-            } else {
-                f = min(f, rest[encode(last, x)]);
-            }
-        }
-        f = min(f, b[last]);
-        b[prvt] += f;
-        for(x = prv[prvt], last = prvt; x != -1; last = x, x = prv[x]) {
-            if(last < x) {
-                rest[encode(x, last)] += f;
-            } else {
-                rest[encode(last, x)] -= f;
-            }
-        }
-        b[last] -= f;
-        ans += f;
+        while(int f = DFS()) ans += f;
     }
     return ans;
 }
