6c8c6630dd7bc5e5a30105176c3dbcb113db22c9
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-01-24 09:10:48 +0800

    add sample data

--- probs/107TPE/lucky/gen.sh
+++ probs/107TPE/lucky/gen.sh
@@ -2,8 +2,8 @@
 
 g++ src/gen.cpp -o gen -std=c++17 -Wall -Wextra && ./gen 7122 -o tests/
 
-# cat src/sample-1.in >tests/10.in
-# cat src/sample-2.in >tests/11.in
+cat src/sample-1.in >tests/01.in
+cat src/sample-2.in >tests/06.in
 
 g++ src/sol.cpp -o sol -std=c++17 -Wall -Wextra -Dlocal -fsanitize=undefined -fsanitize=address -Ofast
 
@@ -13,6 +13,3 @@ done
 
 rm gen
 rm sol
-
-# for i in $(seq -w 0 39); do
-#     ./na
