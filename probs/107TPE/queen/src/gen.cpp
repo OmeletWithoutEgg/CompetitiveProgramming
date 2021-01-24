bb0830489a663701b360f8aedff155c2d2246ca2
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-01-24 10:38:31 +0800

    fix testdata

--- probs/107TPE/queen/src/gen.cpp
+++ probs/107TPE/queen/src/gen.cpp
@@ -20,10 +20,15 @@ void randomGen(string filename, int minn, int maxn, int maxc) {
     cerr << filename << ' ' << n << " ok\n";
 }
 
+Polygon randomPolygon(int n, int C) {
+    int dx = rnd.next(C / 5, C / 2);
+    int dy = rnd.next(C / 5, C / 2);
+    return rndg.convexPolygon(n, -dx, -dy, dx, dy);
+}
 void genBigPolygon(string filename, int minn, int maxn, int maxc) {
     int n = rnd.next(minn/2, maxn/2);
-    auto P1 = rndg.convexPolygon(n, -maxc / 2, maxc / 2);
-    auto P2 = rndg.convexPolygon(n, -maxc / 2, maxc / 2);
+    auto P1 = randomPolygon(n, maxc);
+    auto P2 = randomPolygon(n, maxc);
     auto S1 = rndg.point(-maxc, -maxc, -maxc/2, maxc);
     auto S2 = rndg.point(maxc/2, -maxc, maxc, maxc);
 
@@ -33,8 +38,10 @@ void genBigPolygon(string filename, int minn, int maxn, int maxc) {
     d.polygon(P2.shift(S2));
     d.dumpSvg("polygon.svg");
     */
+    auto P = (P1.shift(S1) + P2.shift(S2)).sorted();
+    P.erase(unique(P.begin(), P.end(), [](Point a, Point b){ return a.x == b.x; }), P.end());
     ofstream fout(filename);
-    fout << (P1.shift(S1) + P2.shift(S2)).shuffled().printN() << '\n';
+    fout << P.choice(P.size() / 2 * 2).shuffle().printN() << '\n';
     fout.close();
     cerr << filename << ' ' << n*2 << " ok\n";
 }
@@ -52,5 +59,5 @@ int main(int argc, char *argv[]) {
     for(int i = 20; i < 30; i++)
         randomGen(prefix + format("%02d.in", i), 20000, 50000, 100000000);
     for(int i = 30; i < 40; i++)
-        randomGen(prefix + format("%02d.in", i), 60000, 100000, 100000000);
+        genBigPolygon(prefix + format("%02d.in", i), 60000, 100000, 100000000);
 }
