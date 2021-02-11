996239e36ddb1b4897464d27bf51d8a8a5091481
OmeletWithoutEgg <2qbingxuan@gmail.com>
2021-02-11 22:31:25 +0800

    IOICAMP probs

--- jizz.cpp
+++ jizz.cpp
@@ -1,63 +1,203 @@
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
+#ifdef local
+#define safe std::cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
 #define debug(args...) qqbx(#args, args)
+#define TAK(args...) std::ostream& operator<<(std::ostream &O, args)
+#define NIE(STL, BEG, END, OUT) template <typename ...T> TAK(std::STL<T...> v) \
+    { O << BEG; int f=0; for(auto e: v) O << (f++ ? ", " : "") << OUT; return O << END; }
+NIE(deque, "[", "]", e) ; NIE(vector, "[", "]", e)
+NIE(set, "{", "}", e) ; NIE(multiset, "{", "}", e) ; NIE(unordered_set, "{", "}", e)
+NIE(map , "{", "}", e.first << ":" << e.second)
+NIE(unordered_map , "{", "}", e.first << ":" << e.second)
+template <typename ...T> TAK(std::pair<T...> p) { return O << '(' << p.first << ',' << p.second << ')'; }
+template <typename T, size_t N> TAK(std::array<T,N> a) { return O << std::vector<T>(a.begin(), a.end()); }
+template <typename ...T> TAK(std::tuple<T...> t) {
+    return O << "(", std::apply([&O](T ...s){ int f=0; (..., (O << (f++ ? ", " : "") << s)); }, t), O << ")";
+}
 template <typename ...T> void qqbx(const char *s, T ...args) {
     int cnt = sizeof...(T);
-    ((std::cerr << "(" << s << ") = (") , ... , (std::cerr << args << (--cnt ? ", " : ")\n")));
+    if(!cnt) return std::cerr << "\033[1;32m() = ()\033\[0m\n", void();
+    (std::cerr << "\033[1;32m(" << s << ") = (" , ... , (std::cerr << args << (--cnt ? ", " : ")\033[0m\n")));
 }
+#else
+#define safe ((void)0)
+#define debug(...) ((void)0)
+#endif // local
+
 #define all(v) begin(v),end(v)
 
 using namespace std;
-const int maxn = 2000;
+const int maxn = 155, mod = 998244353, sigma = 6;
 
-int n, type;
-bool has[maxn];
-bitset<maxn> bs[maxn];
-bool insert(bitset<maxn> b) {
-    for(int i = n-1; i >= 0; i--) if(b[i]) {
-        if(has[i]) {
-            b ^= bs[i];
-        } else {
-            bs[i] = b;
-            has[i] = true;
-            return true;
+constexpr struct FastMod {
+    using ull = uint64_t;
+    using uL = __uint128_t;
+    ull b, m;
+    constexpr FastMod(ull b) : b(b), m(ull((uL(1) << 64) / b)) {}
+    constexpr ull reduce(ull a) const {
+        ull q = (ull)((uL(m) * a) >> 64), r = a - q * b;
+        return r >= b ? r - b : r;
+    }
+} fm(mod);
+
+template <typename T, T MOD> class Modular {
+public:
+    constexpr Modular() : v() {}
+    template <typename U> Modular(const U &u) { v = (0 <= u && u < MOD ? u : (u%MOD+MOD)%MOD); }
+    template <typename U> explicit operator U() const { return U(v); }
+    T operator()() const { return v; }
+#define REFOP(type, expr...) Modular &operator type (const Modular &rhs) { return expr, *this; }
+    REFOP(+=, v += rhs.v - MOD, v += MOD & (v >> width)) ; REFOP(-=, v -= rhs.v, v += MOD & (v >> width))
+    // fits for MOD^2 <= 9e18
+    REFOP(*=, v = fm.reduce(1LL * v * rhs.v)) ; REFOP(/=, *this *= inverse(rhs.v))
+#define VALOP(op) friend Modular operator op (Modular a, const Modular &b) { return a op##= b; }
+    VALOP(+) ; VALOP(-) ; VALOP(*) ; VALOP(/)
+    Modular operator-() const { return 0 - *this; }
+    friend bool operator == (const Modular &lhs, const Modular &rhs) { return lhs.v == rhs.v; }
+    friend bool operator != (const Modular &lhs, const Modular &rhs) { return lhs.v != rhs.v; }
+    friend std::istream & operator>>(std::istream &I, Modular &m) { T x; I >> x, m = x; return I; }
+    friend std::ostream & operator<<(std::ostream &O, const Modular &m) { return O << m.v; }
+private:
+    constexpr static int width = sizeof(T) * 8 - 1;
+    T v;
+    static T inverse(T a) {
+        // copy from tourist's template
+        T u = 0, v = 1, m = MOD;
+        while (a != 0) {
+            T t = m / a;
+            m -= t * a; std::swap(a, m);
+            u -= t * v; std::swap(u, v);
+        }
+        assert(m == 1);
+        return u;
+    }
+};
+
+using Mint = Modular<int, mod>;
+Mint modpow(Mint e, uint64_t p) { Mint r = 1; while(p) (p&1) && (r *= e), e *= e, p >>= 1; return r; } // 0^0 = 1
+
+vector<Mint> BerlekampMassey(vector<Mint> x) {
+    vector<Mint> cur, ls;
+    int lf = 0;
+    Mint ld = 0;
+    for (int i = 0; i < (int)x.size(); ++i) {
+        Mint t = 0;
+        for (int j = 0; j < (int)cur.size(); ++j)
+            t += cur[j] * x[i - j - 1];
+        if (t == x[i]) continue;
+        if (cur.empty()) {
+            cur.resize(i + 1);
+            lf = i, ld = t - x[i];
+            continue;
+        }
+        Mint k = (t - x[i]) / ld;
+        vector<Mint> c(i - lf - 1);
+        c.push_back(k);
+        for (int j = 0; j < (int)ls.size(); ++j)
+            c.push_back(- k * ls[j]);
+        if (c.size() < cur.size()) c.resize(cur.size());
+        for (int j = 0; j < (int)cur.size(); ++j)
+            c[j] += cur[j];
+        if (i - lf + (int)ls.size() >= (int)cur.size()) {
+            ls = cur, lf = i;
+            ld = t - x[i];
+        }
+        cur = c;
+    }
+    return cur;
+    // x[i] = \sum x[i-1-j] * cur[j] for j in len(cur)
+}
+vector<Mint> sparseLinearEquation(vector<vector<pair<int,Mint>>> A, vector<Mint> b) {
+    int n = b.size();
+    vector<Mint> u(n);
+    for (int i = 0; i < n; i++) u[i] = rand();
+
+    auto mulA = [&A, &n](vector<Mint> &v) {
+        vector<Mint> res(n);
+        for (int i = 0; i < n; i++)
+            for (auto [j, c]: A[i])
+                res[i] += v[j] * c;
+        v = res;
+    };
+    vector<Mint> a(n * 2 + 1), cur = b;
+    for (int i = 0; i <= n * 2; i++) {
+        a[i] = inner_product(u.begin(), u.end(), cur.begin(), Mint(0));
+        mulA(cur);
+    }
+    auto r = BerlekampMassey(a);
+    int m = r.size();
+    // debug(r, a);
+    // A^m = A^{m-1} * r[0] + A^{m-2} * r[1] + ... + A^0 * r[m-1]
+    // A^{-1}b = (A^{m-1}b - A^{m-2}b * r[0] - A^{m-3}b * r[1] - ... - A^0b * r[m-2])
+    cur = b;
+    vector<Mint> res(n);
+    for (int i = 0; i < m-1; i++) {
+        for (int j = 0; j < n; j++) res[j] -= cur[j] * r[m-2-i];
+        mulA(cur);
+    }
+    for (int j = 0; j < n; j++) res[j] += cur[j];
+    Mint inv = 1 / r[m-1];
+    for (int j = 0; j < n; j++) res[j] *= inv;
+    return res;
+}
+struct AhoCorasick {
+    int ch[maxn][sigma], fail[maxn];
+    int tot;
+    bool isEnd[maxn];
+    void insert(const string &s) {
+        int now = 0;
+        for (char c: s) {
+            c -= '1';
+            if (!ch[now][c])
+                ch[now][c] = ++tot;
+            now = ch[now][c];
+        }
+        isEnd[now] = true;
+    }
+    void build() {
+        queue<int> q;
+        for (int i = 0; i < sigma; i++)
+            if (ch[0][i])
+                q.push(ch[0][i]);
+        while (!q.empty()) {
+            int i = q.front(); q.pop();
+            int f = fail[i];
+            isEnd[i] |= isEnd[f];
+            for (int c = 0; c < sigma; c++) {
+                if (ch[i][c]) {
+                    fail[ ch[i][c] ] = ch[f][c];
+                    q.push(ch[i][c]);
+                } else {
+                    ch[i][c] = ch[f][c];
+                }
+            }
         }
     }
-    return false;
-}
-int addBit(bitset<maxn> b) {
-    for(int i = n-1; i >= 0; i--) if(!has[i]) {
-        b[i] = !b[i];
-        assert( insert(b) );
-        return i;
+    void solve() {
+        vector<vector<pair<int,Mint>>> A(tot+1);
+        vector<Mint> B(tot+1);
+        Mint inv = 1 / Mint(sigma);
+        for (int i = 0; i <= tot; i++) {
+            A[i].emplace_back(i, 1);
+            for (int c = 0; c < sigma; c++)
+                if (int j = ch[i][c]; !isEnd[j])
+                    A[i].emplace_back(j, -inv);
+        }
+        for (int i = 0; i <= tot; i++) if (!isEnd[i]) B[i] = 1;
+        cout << sparseLinearEquation(A, B)[0] << '\n';
     }
-    abort();
-}
+} AC;
 signed main() {
+    // debug(BerlekampMassey({ 1, 1, 3, 7, 17 }));
     ios_base::sync_with_stdio(0), cin.tie(0);
-    cin >> n >> type;
-    vector<bool> isBasis(n);
-    vector<string> ans(n);
-    for(int i = 0; i < n; i++) {
+    int n;
+    cin >> n;
+    while (n--) {
         string s;
         cin >> s;
-        ans[i] = s;
-        if(insert(bitset<maxn>(s))) {
-            isBasis[i] = true;
-        }
+        AC.insert(s);
     }
-    if (n == 1 && type == 2)
-        return cout << ans[0] << '\n', 0;
-    int cnt = 0;
-    for(int i = 0; i < n; i++) {
-        if(!isBasis[i]) {
-            int pos = addBit(bitset<maxn>(ans[i]));
-            ans[i][n-1-pos] ^= 1;
-            ++cnt;
-        }
-    }
-    if(type == 1)
-        cout << cnt << '\n';
-    else if(type == 2)
-        for(int i = 0; i < n; i++)
-            cout << ans[i] << '\n';
+    AC.build();
+    AC.solve();
 }
--- tmp.cpp
+++ tmp.cpp
@@ -1,39 +1,256 @@
+#pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
-using namespace std;
-#define maxn 200005
-
-int n;
-pair<int, int> p[maxn];
-long double angle, cos_v, sin_v;
-
-bool cmp(pair<int, int> a, pair<int, int> b) {
-    auto v1 = make_pair(a.first * cos_v - a.second * sin_v
-                      , a.first * sin_v + a.second * cos_v);
-    auto v2 = make_pair(b.first * cos_v - b.second * sin_v
-                      , b.first * sin_v + b.second * cos_v);
-    return v1 < v2;
+#ifdef local
+#define debug(x...) qqbx(#x, x)
+template <class ...T> void qqbx(const char *s, T ...a) {
+    int f = 0;
+    ((std::cerr << "(" << s << ") = (") , ... , (std::cerr << (f++ ? ", " : "") << a)), std::cerr << ")\n";
 }
+#else
+#define debug(...) ((void)0)
+#endif // local
+#define pb emplace_back
+#define all(v) begin(v),end(v)
 
-int main() {
-    cin.tie(0), cout.sync_with_stdio(0);
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); // random number generator
-    cin >> n;
-    for (int i = 1; i <= n; ++ i)
-        cin >> p[i].first >> p[i].second;
+using namespace std;
+using ll = int64_t;
+const int maxn = 250025, inf = 1e9;
 
-    long long ans = 6e18, k = 200;
-    angle = rng();
-    cos_v = cos(angle);
-    sin_v = sin(angle);
-    sort(p + 1, p + 1 + n, cmp);
-    for (int i = 1; i <= n; ++ i) { // Sweep backward k points
-        for (int j = i + 1; j <= i + k && j <= n; ++ j) {
-            long long v1 = (p[i].first - p[j].first);
-            long long v2 = (p[i].second - p[j].second);
-            long long dis = v1 * v1 + v2 * v2;
-            ans = min(ans, dis);
+struct RMQ {
+    int mn[20][maxn];
+    int & operator [](int i) { return mn[0][i]; }
+    void build(int n) {
+        for (int L = 1; L < 20; L++) {
+            int len = 1 << L-1;
+            for (int i = 0; i < n; i++)
+                mn[L][i] = min(mn[L-1][i], (i + len < n ? mn[L-1][i + len] : inf));
         }
     }
-    cout << ans << "\n";
-    return 0;
+    int operator()(int l, int r) const {
+        if (l == r) return inf;
+        // assert( l < r );
+        int lev = __lg(r - l);
+        return min(mn[lev][l], mn[lev][r - (1 << lev)]);
+    }
+};
+
+struct SuffixArray {
+    int sa[maxn], rk[maxn];
+    int nrk[maxn];
+    int lcp[maxn];
+    string s;
+    RMQ LCP, pos;
+    void build(const string &s_) {
+        s = s_;
+        for (int i = 0; i < s.size(); i++) rk[i] = s[i], sa[i] = i;
+        for (int L = 1; L < s.size(); L <<= 1) {
+            auto cmp = [&](int a, int b) {
+                if (rk[a] != rk[b]) return rk[a] < rk[b];
+                int ra = a+L < s.size() ? rk[a+L] : -1;
+                int rb = b+L < s.size() ? rk[b+L] : -1;
+                return ra < rb;
+                if (ra != rb) return ra < rb;
+                return a > b;
+            };
+            sort(sa, sa+s.size(), cmp);
+            nrk[sa[0]] = 0;
+            for (int i = 1; i < s.size(); i++) nrk[sa[i]] = nrk[sa[i-1]] + cmp(sa[i-1], sa[i]);
+            for (int i = 0; i < s.size(); i++) rk[i] = nrk[i];
+        }
+        // cerr << "SA of " << s << '\n';
+        // for (int i = 0; i < s.size(); i++) cerr << i << ' ' << s.substr(sa[i]) << '\n';
+        for (int i = 0, h = 0; i < s.size(); i++) {
+            if (rk[i] == 0) continue;
+            int j = sa[rk[i]-1];
+            while (i+h < s.size() && j+h < s.size() && s[i+h] == s[j+h]) ++h;
+            lcp[rk[i]-1] = h;
+            if (h > 0) --h;
+        }
+        // for (int i = 0; i < s.size(); i++)
+        //     cerr << lcp[i] << ' ' << s.substr(sa[i]) << '\n';
+        for (int i = 0; i < s.size(); i++) LCP[i] = lcp[i];
+        LCP.build(s.size());
+        for (int i = 0; i < s.size(); i++) pos[i] = inf;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '$') break;
+            pos[rk[i]] = -i;
+            // debug(rk[i], s[i]);
+        }
+        // debug("SAFE\n");
+        pos.build(s.size());
+    }
+
+    pair<int,int> bsearch(int suf, int p) {
+        assert ( sa[suf] + p <= s.size() );
+        int l = suf, r = suf - 1;
+        for (int step = 1 << 18; step; step >>= 1) {
+            if (l - step >= 0 && LCP(l - step, suf) >= p) l -= step;
+            if (r + step < s.size() && LCP(suf, r + step) >= p) r += step;
+        }
+        ++r;
+        // debug(suf, l, r);
+        // debug(s.substr(sa[suf]), p);
+        // for (int i = 0; i < s.size(); i++) cerr << i << ' ' << sa[i] << ' ' << s.substr(sa[i]) << '\n';
+        return { l, r };
+        // returns [l, r) such that LCP(suf, sa[i]) >= p for all i \in [l, r)
+    }
+
+    int occur(int suf, int p) {
+        auto [l, r] = bsearch(suf, p);
+        int mx = -pos(l, r);
+        if (mx == -inf) return -1;
+        return mx;
+    }
+} SA, rSA;
+
+namespace cdq {
+    struct Fenwick {
+        int sum[maxn * 2];
+        void add(int p, int d) {
+            for (p += maxn; p < maxn * 2; p += p & -p) sum[p] += d;
+        }
+        int qry(int p) {
+            int res = 0;
+            for (p += maxn; p > 0; p -= p & -p) res += sum[p];
+            return res;
+        }
+    } BB;
+    struct Query {
+        int t;
+        int x, y, z;
+        int v;
+        int id;
+    } Q[maxn * 4];
+    int tot;
+    int64_t ans[maxn];
+    void addQ(int t, int x, int y, int z, int v, int id) {
+        Q[tot++] = {t, x, y, z, v, id};
+    }
+    bool cmp(const Query &a, const Query &b) {
+        return a.y != b.y ? a.y < b.y : a.t < b.t;
+    }
+    void solve(int l, int r) {
+        /*
+        for (int i = l; i < r; i++) {
+            if (Q[i].t == 1) {
+                for (int j = l; j < i; j++) {
+                    if (Q[j].t == 0 && Q[j].y <= Q[i].y && Q[j].z <= Q[i].z) {
+                        ans[Q[i].id] += Q[j].v * Q[i].v;
+                    }
+                }
+            }
+        }
+        return;
+        */
+        if (r-l <= 1) return;
+        int m = (l+r)/2;
+        solve(l, m);
+        solve(m, r);
+
+        int i = l, j = m;
+        while (i != m || j != r) {
+            if (j == r || (i != m && cmp(Q[i], Q[j]))) {
+                auto &qq = Q[i++];
+                if (qq.t == 0)
+                    BB.add(qq.z, qq.v);
+            } else {
+                auto &qq = Q[j++];
+                if (qq.t == 1)
+                    ans[qq.id] += BB.qry(qq.z) * qq.v;
+            }
+        }
+        for (int i = l; i < m; i++) {
+            auto &qq = Q[i];
+            if (qq.t == 0) BB.add(qq.z, -qq.v);
+        }
+        inplace_merge(Q+l, Q+m, Q+r, cmp);
+    }
+    void solve() {
+        sort(Q, Q+tot, [](const Query &a, const Query &b){ return a.x != b.x ? a.x < b.x : a.t < b.t; });
+        solve(0, tot);
+    }
+}
+
+// vector<tuple<int,int,int>> cdq;
+void addQuery(int l1, int r1, int l2, int r2, int l3, int r3, int id) {
+    cdq::addQ(1, r1, r2, r3, 1, id);
+    
+    cdq::addQ(1, l1, r2, r3, -1, id);
+    cdq::addQ(1, r1, l2, r3, -1, id);
+    cdq::addQ(1, r1, r2, l3, -1, id);
+
+    cdq::addQ(1, l1, l2, r3, 1, id);
+    cdq::addQ(1, r1, l2, l3, 1, id);
+    cdq::addQ(1, l1, r2, l3, 1, id);
+
+    cdq::addQ(1, l1, l2, l3, -1, id);
+    // debug(l1, r1, l2, r2, l3, r3);
+    /*
+    int ans = 0;
+    for (auto [x, y, z]: cdq)
+        if (l1 <= x && x < r1 &&
+            l2 <= y && y < r2 &&
+            l3 <= z && z < r3)
+            ++ans;
+    // debug(ans);
+    return ans;
+    */
+}
+void calc(int n, int offset, int sz, int id) {
+    int l1 = SA.occur(SA.rk[offset], sz);
+    int r1 = n - 1 - rSA.occur(rSA.rk[offset], sz);
+    for (int i = 1; i < sz; i++) {
+        int j = sz - i;
+        auto [l2, r2] = SA.bsearch(SA.rk[offset + i], j);
+        auto [l3, r3] = rSA.bsearch(rSA.rk[offset + j], i);
+        if (l1 >= r1 || l2 >= r2 || l3 >= r3) continue;
+        addQuery(l1-1, r1-1, l2-1, r2-1, l3-1, r3-1, id);
+    }
+}
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    string orgS, orgT;
+    cin >> orgS >> orgT;
+
+    int n = orgS.size();
+
+    int q;
+    cin >> q;
+    string S = orgS, T = orgT;
+    reverse(all(S));
+    
+    vector<int> pos(q), sz(q);
+    for (int i = 0; i < q; i++) {
+        string P;
+        cin >> P;
+        pos[i] = T.size() + 1;
+        sz[i] = P.size();
+        T += '$' + P;
+        reverse(all(P));
+        S += '$' + P;
+    }
+    SA.build(T);
+    rSA.build(S);
+
+    for (int i = 0; i+1 < n; i++) {
+        int x = SA.rk[i+1];
+        int y = rSA.rk[n-1-i];
+        // debug(SA.s.substr(i+1));
+        // debug(rSA.s.substr(n-1-i));
+        cdq::addQ(0, i, x, y, 1, -1);
+        // cdq.pb(i, x, y);
+        // debug(i, x, y);
+    }
+    //debug(S, T);
+
+    S = orgS, T = orgT;
+    reverse(all(T));
+
+    for (int i = 0; i < q; i++) {
+        // debug(l, r);
+        calc(n, pos[i], sz[i], i);
+    }
+    cdq::solve();
+    for (int i = 0; i < q; i++)
+        cout << cdq::ans[i] << '\n';
 }
