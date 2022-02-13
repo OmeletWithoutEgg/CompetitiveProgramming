4eabc074f935fd689a9ab108b617da0c3546ba82
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-02-13 22:23:10 +0800

    gen testdata of tioj 1841

--- gen.cpp
+++ gen.cpp
@@ -3,7 +3,7 @@
 #define debug(args...) qqbx(#args, args)
 template <typename ...T> void qqbx(const char *s, T ...args) {
     int cnt = sizeof...(T);
-    ((std::cerr << "(" << s << ") = (") , ... , (std::cerr << args << (--cnt ? ", " : ")\n")));
+    ((std::cerr << "\e[032;1m(" << s << ") = (") , ... , (std::cerr << args << (--cnt ? ", " : ")\e[0m\n")));
 }
 #else
 #define debug(...) ((void)0)
@@ -15,13 +15,43 @@ using namespace std;
 const int maxn = 100025, mod = 998244353, inf = 1e9, maxc = 100025;
 const int64_t INF = 1e18;
 
-signed main() {
+signed main(int argc, char **argv) {
+    assert (argc >= 2);
     ios_base::sync_with_stdio(0), cin.tie(0);
-    mt19937 rng(18392);
-    const int n = 100;
-    const int R = 32;
-    const int C = 10000;
-    cout << n << ' ' << R << '\n';
-    for (int i = 0; i < n; i++)
-        cout << uniform_int_distribution<int>(1, C)(rng) << (i+1==n ? '\n' : ' ');
+    const int seed = atoi(argv[1]);
+    mt19937 rng(seed);
+
+    vector<vector<int>> perms;
+    int cnt = 0;
+    for (int N = 2; N <= 8; N++) {
+        vector<int> perm(N);
+        iota(all(perm), 0);
+        do {
+            perms.emplace_back(perm);
+            cnt += perm.size();
+        } while (next_permutation(all(perm)));
+    }
+
+    // for (int it = 0; it < 5; it++) {
+    //     int N = 1000000;
+    //     vector<int> p(N);
+    //     for (int i = 0; i < N; i++) {
+    //         p[i] = (i & 1 ? N - i/2 : i/2);
+    //     }
+    //     cnt += N;
+    //     perms.emplace_back(p);
+    // }
+
+    shuffle(perms.begin(), perms.end(), rng);
+
+    int T = perms.size();
+    debug(cnt, T);
+    cout << T << '\n';
+    for (const auto &perm: perms) {
+        int N = perm.size() - 1;
+        cout << N << '\n';
+        for (int i = 1; i <= N; i++) {
+            cout << perm[i] - perm[i-1] << (i==N ? '\n' : ' ');
+        }
+    }
 }
